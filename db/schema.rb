9a1ab98c249cf63427e53861ba350ab394b94e3c
Octave Laventure <octave.laventure@jobteaser.com>
2020-11-15 01:07:25 +0100

    use pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_14_212730) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.string "slug"
